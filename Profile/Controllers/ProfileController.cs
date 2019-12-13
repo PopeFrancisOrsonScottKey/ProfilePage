c18a9ff358f46bdc159dd8f72dd97a73beb52710
Jacob Ramsey-Smith <jrsrj13@gmail.com>
2019-12-13 16:57:47 -0600

    project framework

---
+++ Profile/Controllers/ProfileController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Profile.Models;
+
+namespace Profile.Controllers
+{
+    public class ProfileController : Controller
+    {
+        [HttpGet(Name = "profile")]
+        public ActionResult Profile()
+        {
+            ProfileModel model = new ProfileModel();
+            return View(model);
+        }
+    }
+}
