41840bdf9a8e7a360315732a496b4d5d38140e3a
Jacob Ramsey-Smith <jrsrj13@gmail.com>
2019-12-14 13:13:44 -0600

    header fixes and timer on home

--- Profile/wwwroot/js/site.js
+++ Profile/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var span = document.getElementById('DateTime');
+
+function time() {
+    var d = new Date();
+    span.innerHTML = d.toLocaleString();
+} setInterval(time, 1000);
+
+window.onload = time();
